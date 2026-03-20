13f5f2aa5ea9a4a42c6f09014e892f981c1745d0
Raphael Rios <rios.raph@gmail.com>
2026-03-19 22:04:46 -0300

    Initial commit: Booking Entity created

---
+++ src/main/java/com/system/bookingservice/model/Booking.java
@@ -0,0 +1,27 @@
+package com.system.bookingservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String clientName;
+    private String serviceType;
+    private LocalDateTime appointmentDate;
+    private String status;
+}
