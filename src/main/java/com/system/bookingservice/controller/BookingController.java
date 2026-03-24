31846ad66404b9a3f01a755cc22b5cab3946aa72
Raphael Rios <rios.raph@gmail.com>
2026-03-24 12:09:16 -0300

    feat: implement GET all bookings endpoint in BookingController

---
+++ src/main/java/com/system/bookingservice/controller/BookingController.java
@@ -0,0 +1,21 @@
+package com.system.bookingservice.controller;
+
+import com.system.bookingservice.model.Booking;
+import com.system.bookingservice.repository.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bookings")
+public class BookingController {
+
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    @GetMapping
+    public List<Booking> getAllBookings() {
+        return bookingRepository.findAll();
+    }
+}
